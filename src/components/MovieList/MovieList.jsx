d28d5203c5246540253147cc2920473d156e5718
Liubko89 <142455025+Liubko89@users.noreply.github.com>
2024-04-05 19:18:30 +0100

    removed console logs

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -2,8 +2,6 @@ import { Link, useLocation } from "react-router-dom";
 import css from "./MovieList.module.css";
 
 const MovieList = ({ moviesList }) => {
-  console.log(moviesList);
-
   const location = useLocation();
 
   return (
